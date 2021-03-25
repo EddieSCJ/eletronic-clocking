2a8c47e3ddf8d28c012f2794961aa22b0acbcecf
EddieSCJ <eddiestudyifs@gmail.com>
2021-03-25 00:51:32 -0300

    Password unit test

---
+++ src/test/kotlin/com/edcleidson/electronicclocking/domain/PasswordTest.kt
@@ -0,0 +1,18 @@
+package com.edcleidson.electronicclocking.domain
+
+import com.edcleidson.electronicclocking.utils.helpers.BasicOperationsHelper
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class PasswordTest {
+
+    private val PASSWORD_STRING = "123456"
+
+    @Test
+    fun shouldGenerateAHashPasswordEqualsToProvidedPassword() {
+        val password = Password(PASSWORD_STRING)
+        val hash = password.generateBcryptPassword()
+        assertTrue(BasicOperationsHelper.bCryptPasswordEncoder.matches(PASSWORD_STRING, hash))
+    }
+
+}
